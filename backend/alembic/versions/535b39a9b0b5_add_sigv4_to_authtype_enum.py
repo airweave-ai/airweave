1db409fd341d678aae70d946f4ce9c4fe5cd5276
Bill Nguyen Ton Hoang <bill.nguyentonhoang@live.com>
2025-04-22 09:59:54 +0100

    feat: db migration to add "sigv4" to auth type enum and poetry lock for `aioboto3`

---
+++ backend/alembic/versions/535b39a9b0b5_add_sigv4_to_authtype_enum.py
@@ -0,0 +1,24 @@
+"""Add sigv4 to authtype enum
+
+Revision ID: 535b39a9b0b5
+Revises: e2819d7d0588
+Create Date: 2025-04-22 02:20:18.737543
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '535b39a9b0b5'
+down_revision = 'e2819d7d0588'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
