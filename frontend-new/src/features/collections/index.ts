36735a3ebb99ec509bf3da8eabe8023738085b92
Roch Goszczyński <rg@tonik.com>
2026-03-31 16:33:33 +0200

    feat: load collection list data on collections page

--- frontend-new/src/features/collections/index.ts
+++ frontend-new/src/features/collections/index.ts
@@ -1,2 +1,3 @@
 export { CollectionsSummaryCard } from './components/collections-summary-card';
 export { CollectionsTable } from './components/collections-table';
+export { ensureListCollections } from './api';
--- frontend-new/src/routes/_authenticated/_app/collections.tsx
+++ frontend-new/src/routes/_authenticated/_app/collections.tsx
@@ -1,8 +1,10 @@
 import { CollectionsPage } from '@/app/pages/collections';
+import { ensureListCollections } from '@/features/collections';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authenticated/_app/collections')({
   component: RouteComponent,
+  loader: ({ context }) => ensureListCollections(context),
 });
 
 function RouteComponent() {
